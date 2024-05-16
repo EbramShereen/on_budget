4fd5a8f659479bfff79f8b22e6139e5c88b7dd23
EbramShereen <ebramshereen2002@gmail.com>
2024-05-16 05:19:57 +0300

    delete files

--- lib/service/chatbot_service.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:google_generative_ai/google_generative_ai.dart';
-
-class ChatBotService {
-  final model = GenerativeModel(
-      model: 'gemini-pro', apiKey: 'AIzaSyCPuOgvdAmejhQctaH1W-NGAZ92gKHOq_E');
-}
