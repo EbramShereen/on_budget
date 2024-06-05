1e1a1b9e6e64dc18084f28c330a99cfe6d93cc36
EbramShereen <ebramshereen2002@gmail.com>
2024-06-05 07:18:26 +0300

    delete files

--- lib/src/utils/helper/api/chat_bot.dart
+++
@@ -1,5 +0,0 @@
-//gemini api
-const String kChatBotVersion = 'gemini-pro';
-const String kChatBotApiKey = 'AIzaSyBsHMNVxOhAr0utuGH5f7-I_pvd6TBQd2g';
-
-const String kChatBot = 'lib/src/res/images/chatbot/chatbot.png';
--- lib/src/utils/helper/api/generate_image.dart
+++
@@ -1,4 +0,0 @@
-//generate image api and token
-const String kGenerateImageApi =
-    'https://api-inference.huggingface.co/models/artificialguybr/TshirtDesignRedmond-V2';
-const String kGenerateImageToken = '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';
