0abf2057f015a8ea982c58db940af6b9aa1f99a5
EbramShereen <ebramshereen2002@gmail.com>
2024-06-05 07:23:34 +0300

    make background widget responsive

--- lib/src/utils/components/background.dart
+++ lib/src/utils/components/background.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import '../helper/constants/colors.dart';
 
 class Background extends StatelessWidget {
-  const Background({super.key, required this.child});
+  const Background(
+      {super.key,
+      required this.child,
+      this.horizontalPadding,
+      this.verticalPadding});
 
   final Widget child;
+  final double? horizontalPadding;
+  final double? verticalPadding;
 
   @override
   Widget build(BuildContext context) {
@@ -13,21 +21,22 @@ class Background extends StatelessWidget {
       width: double.infinity,
       height: double.infinity,
       child: Container(
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20))
+              .w,
           color: kThirdColor,
         ),
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 20,
-          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: horizontalPadding ?? 20,
+            vertical: verticalPadding ?? 20,
+          ).r,
           child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(
                 Radius.circular(20),
-              ),
+              ).w,
             ),
             child: child,
           ),
